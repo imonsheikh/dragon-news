77d4f5772d353ad0399c4b9539448f6edeae7a14
imon-sheikh <mdimonhossain2017@gmail.com>
2024-11-19 00:06:29 +0600

    added marquee

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import logo from '../assets/logo.png'
 const Header = () => {
     return (
@@ -6,6 +7,9 @@ const Header = () => {
                 <img className='w-[300px] ' src={logo} alt="Logo" />
             </div>
             <h2 className='text-gray-400'>Journalism Without Fear or Favour</h2>
+            <p>
+                {moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}
+            </p>
         </div>
     );
 };
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
+import LatestNews from '../components/LatestNews';
 
 const HomeLayout = () => {
     return (
         <div>
             <header>
                 <Header></Header>
+                <section className='w-11/12 mx-auto'>
+                  <LatestNews></LatestNews>
+                </section>
             </header>
         </div>
     );
 };
 
-export default HomeLayout;
+export default HomeLayout;    
