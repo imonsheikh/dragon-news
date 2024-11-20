4e4d64bae10dc70018e13481dbd8b25214370e4e
imon-sheikh <mdimonhossain2017@gmail.com>
2024-11-20 08:29:23 +0600

    implement news card

--- src/components/pages/CategoryNews.jsx
+++ src/components/pages/CategoryNews.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import NewsCard from '../NewsCard';
 
 const CategoryNews = () => {
 
-    const {data} = useLoaderData()
-    console.log(data);
+    const {data: news} = useLoaderData()
+    // console.log(news);
     
 
     return (
         <div>
-            category newsssss {data.length} sd
+            <h2 className='font-semibold mb-3'>Dragon News home</h2>
+            <p className='text-gray-400 text-sm'>{news.length} News Found on this category</p>
+            <div>
+            {
+               news.map(singleNews => <NewsCard 
+                key={singleNews.id}
+                news={singleNews}
+                >{singleNews.title}</NewsCard>) 
+                   
+            }
+            </div>
         </div>
     );
 };
